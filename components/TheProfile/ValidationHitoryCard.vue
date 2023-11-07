42bc718152d970e04f8e4879fd280adb191dd885
fardin <vahdatfardin@gmail.com>
2023-11-07 13:24:57 +0330

    validation history in profile corrected

--- components/TheProfile/ValidationHitoryCard.vue
+++ components/TheProfile/ValidationHitoryCard.vue
@@ -4,7 +4,11 @@
   >
     <div class="overflow-hidden w-full h-[200px] rounded-xl rounded-b-none">
       <img
-        :src="state.image ? state.image : '/no-image.png'"
+        :src="
+          state.related_object.image
+            ? state.related_object.image
+            : '/no-image.png'
+        "
         class="w-full h-full object-contain ml-auto hover:scale-105 bg-Gray-b5 dark:bg-LightGray-b5 rounded-b-none"
       />
     </div>
@@ -14,28 +18,33 @@
           <h5
             class="lg:text-lg text-base text-Gray-b5 dark:text-LightGray-b5 line-clamp-1"
           >
-            {{ CommitteeType[state[0] || state.committeeTypeId] }}
+            {{ state.related_object.name }}
           </h5>
           <span
             class="text-xs text-Gray-b4 dark:text-LightGray-b4 text-end flex items-center"
-            >{{ state[2] || state.totalMembers }} <i class="isax isax-user"
+            >{{ state.total_members }} <i class="isax isax-user"
           /></span>
         </div>
-        <img
-          src="/trending-collections/mrfox-avatar.png"
-          class="h-10 w-10 rounded-full"
-        />
+        <div class="flex justify-between items-center">
+          <img
+            src="/trending-collections/mrfox-avatar.png"
+            class="h-10 w-10 rounded-full"
+          />
+          <span class="bg-Primary bg-opacity-20 text-Primary px-2 rounded-xl text-xs font-semibold">
+            {{ state.related_object.status }}
+          </span>
+        </div>
       </div>
       <div class="flex justify-between items-center mt-4">
         <div class="flex flex-col gap-2 items-center">
           <p class="md:text-base text-sm text-green-800">
-            {{ state[3] || state.yesVotes }}
+            {{ state.yes_votes }}
             <i class="isax isax-like-1 text-xl text-green-800" />
           </p>
         </div>
         <div class="flex flex-col gap-2 items-center">
           <p class="md:text-base text-sm text-red-800">
-            {{ state[4] || state.noVotes }}
+            {{ state.no_votes }}
             <i class="isax isax-dislike text-xl text-red-800" />
           </p>
         </div>
