9ee8de2d2c1d112288d4340af1578f0c4c2a9702
fardin <vahdatfardin@gmail.com>
2024-01-01 02:59:55 +0330

    profile multi lang added

--- locales/en.js
+++ locales/en.js
@@ -1,9 +1,11 @@
 import header from "./en/header.json";
 import footer from "./en/footer.json";
 import landing from "./en/landing.json";
+import profile from "./en/profile.json";
 
 export default {
   ...header,
   ...footer,
   ...landing,
+  ...profile
 };
--- locales/fa.js
+++ locales/fa.js
@@ -1,9 +1,11 @@
 import header from "./fa/header.json";
 import footer from "./fa/footer.json";
 import landing from "./fa/landing.json";
+import profile from "./fa/profile.json";
 
 export default {
   ...header,
   ...footer,
   ...landing,
+  ...profile,
 };
